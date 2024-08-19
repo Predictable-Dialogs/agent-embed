1b5ff9eac95f42d5ddfd674fe540dfa2e3da37e0
Jai <jaikant@gmail.com>
2024-08-19 16:45:51 +0530

    support markdown in text

--- js/src/features/blocks/bubbles/textBubble/components/plate/PlateText.tsx
+++ js/src/features/blocks/bubbles/textBubble/components/plate/PlateText.tsx
@@ -1,30 +1,21 @@
-import { Show } from 'solid-js'
+import { createSignal, onMount } from 'solid-js';
+import { marked } from 'marked';
+import DOMPurify from 'dompurify';
 
 export type PlateTextProps = {
   text: string
-  bold?: boolean
-  italic?: boolean
-  underline?: boolean
 }
 
-const computeClassNames = (
-  bold: boolean | undefined,
-  italic: boolean | undefined,
-  underline: boolean | undefined
-) => {
-  let className = ''
-  if (bold) className += 'slate-bold'
-  if (italic) className += ' slate-italic'
-  if (underline) className += ' slate-underline'
-  return className
-}
+export const PlateText = (props: PlateTextProps) => {
+  const [htmlContent, setHtmlContent] = createSignal('');
 
-export const PlateText = (props: PlateTextProps) => (
-  <Show
-    when={computeClassNames(props.bold, props.italic, props.underline)}
-    keyed
-    fallback={<>{props.text}</>}
-  >
-    {(className) => <span class={className}>{props.text}</span>}
-  </Show>
-)
+  onMount(async () => {
+    const markdown = await marked(props.text);
+    const safeHTML = DOMPurify.sanitize(markdown);
+    setHtmlContent(safeHTML);
+  });
+
+  return (
+    <span innerHTML={htmlContent()} />
+  )
+}
